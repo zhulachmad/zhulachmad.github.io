197ac557e0ec48eaf397160735d70891c9707254
sisfob <zhulachmad55@gmail.com>
2017-07-09 06:48:03 +0800

    testing

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;">
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache">
+<title>IP CAMERA</title>
+<link href="style/css.css" rel="stylesheet" type="text/css">
+<SCRIPT language="JavaScript" src="ipcamera.js"></SCRIPT>
+</head>
+<body>
+<IMG NAME="webcam" SRC="http://192.168.3.254:10001/jpg/image.jpg?1422104035771" BORDER=0/>
+</body>
+</html>
